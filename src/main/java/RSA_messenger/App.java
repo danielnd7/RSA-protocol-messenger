dd68d8d4b7db552bcff26e1ab24fef8e8bbb1d76
DanielNemchenko <danielnemchenko8@gmail.com>
2024-08-07 20:28:05 +0200

    New methods in App and Test

--- src/main/java/RSA_messenger/App.java
+++ src/main/java/RSA_messenger/App.java
@@ -4,9 +4,21 @@ public class App {
     // Instance variables
     private Server server;
     private PrivateUser privateUser;
+
+    // Constructor
+    //
+
+
     // All methods
-    public void CreateUser(){
-        //  Creates new PrivateUser for this class and PublicUser for the server
+    public void loadServerFromFile(){
+        // if there is no the file, setServer will be called
+    }
+    public void loadPrivateUserFromFile(){
+        //... In case of absence, calls CreateUser
+    }
+
+    public void setServer(Server server) {
+        this.server = server;
     }
 
     public void send(){
@@ -14,5 +26,10 @@ public class App {
     }
     public void read(){
 
+    }
+
+    // Auxiliary methods
+    public void createUser(){
+        //  Creates new PrivateUser for this class and PublicUser for the server
     }
 }
--- src/main/java/RSA_messenger/Test.java
+++ src/main/java/RSA_messenger/Test.java
@@ -4,7 +4,23 @@ public class Test
 {
     public static void main( String[] args )
     {
+        System.out.println( "Welcome to RSA-protocol-messenger!" );
 
-        System.out.println( "Hello World!" );
+        // Preparing for work
+        App app = new App();
+        app.loadServerFromFile();
+        app.loadPrivateUserFromFile();
+
+        // while
+        System.out.println("What would you like to do?");
+        System.out.println("(1) To Read your messages");
+        System.out.println("(2) To Write a message");
+        System.out.println("(3) Finish");
+
+
+
+
+
+
     }
 }
