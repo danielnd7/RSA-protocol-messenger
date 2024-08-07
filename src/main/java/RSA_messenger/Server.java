ba8112dbf912c88f376fcc4704be73ed2e45e7e0
darchery <anestardiaz@gmail.com>
2024-08-07 21:46:29 +0200

    Created 2 maps to control the user's messages

--- src/main/java/RSA_messenger/Server.java
+++ src/main/java/RSA_messenger/Server.java
@@ -1,8 +1,13 @@
 package RSA_messenger;
 
 import java.util.List;
+import java.util.Map;
 
 public class Server { // Server mock
+
+    private Map<PublicUser, List<String>> receivedMessages;
+    private Map<PublicUser, List<String>> sentMessages;
+
     private List<PublicUser> usersList;
     private List<Message> allMessages;
 
