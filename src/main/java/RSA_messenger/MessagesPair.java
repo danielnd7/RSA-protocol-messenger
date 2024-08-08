4655826b49744aa11b991eef589811ef0f249739
darchery <anestardiaz@gmail.com>
2024-08-08 13:04:14 +0200

    Added a constructor and getter and setter to MessagesPairClass

--- src/main/java/RSA_messenger/MessagesPair.java
+++ src/main/java/RSA_messenger/MessagesPair.java
@@ -1,11 +1,29 @@
 package RSA_messenger;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MessagesPair {
-    public List<Message> receivedMessages;
-    public List<Message> sentMessages;
+    private List<Message> receivedMessages;
+    private List<Message> sentMessages;
+    
 
-    // It's better make the lists private and add get and set methods
+    public MessagesPair() {
+        receivedMessages = new ArrayList<>();
+        sentMessages = new ArrayList<>();
+    }
 
+    public List<Message> getReceivedMessages() {
+        return receivedMessages;
+    }
+    public void setReceivedMessages(List<Message> receivedMessages) {
+        this.receivedMessages = receivedMessages;
+    }
+
+    public List<Message> getSentMessages() {
+        return sentMessages;
+    }
+    public void setSentMessages(List<Message> sentMessages) {
+        this.sentMessages = sentMessages;
+    }
 }
