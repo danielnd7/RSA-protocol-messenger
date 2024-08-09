26903c083fd5ad67b932bad407a7a76037fa734c
darchery <anestardiaz@gmail.com>
2024-08-09 18:02:47 +0200

    Update of send method
    These method only need to organise the message (to be sent and to be received), to the private user and the public one, maybe a new super class User is the solution with 2 subclasses ,the private and the public one.

--- src/main/java/RSA_messenger/App.java
+++ src/main/java/RSA_messenger/App.java
@@ -1,7 +1,7 @@
 package RSA_messenger;
 
 import java.util.*;
-import java.util.random.*; // Provisional
+
 
 public class App {
     // Instance variables
@@ -12,19 +12,17 @@ public class App {
 
     // Constructor
     public App() {
-        Server server = new Server();
-
+        server = new Server();
         // If the private user its null create a new one
-        if (privateUser.getUserName().equals(null)) {
-            createUser();
-        }
+        createUser();
+
     }
 
 
     // All methods
     public void loadServerFromFile(){
         // if there is no the file, setServer will be called
-        Server server1 = new Server();
+        //Server server1 = new Server();
     }
     public void loadPrivateUserFromFile(){
         //... In case of absence, calls CreateUser
@@ -35,7 +33,6 @@ public class App {
     }
 
     public void send(){
-
         addRandomsUsers();
 
         // Server has a new method getUsersSet which returns a sorted set of users
@@ -58,8 +55,8 @@ public class App {
             if (readerScanner.hasNextInt()) {
                 contactIndex = readerScanner.nextInt() - 1; // We show from 1 to n contacts, so the real index its n-1
             }
-            String receiverName = nameOfTheReceiver(server.getUsersSet(), contactIndex);
-            System.out.println("\nType a message for " +  receiverName + " : \n");
+            String receiverName = nameOfTheReceiver(server.getUsersSet(),contactIndex);
+            System.out.println("\nType a message for " +  receiverName + " : ");
             // I created nameOfReceiver because i dont find a better way to select an element from the set, cause
             // we cannot do like this: server.gerUserSet.get(index) because there no index on sets
 
@@ -75,7 +72,30 @@ public class App {
 
             // Need to establish the message, put in the map the receiver and the message in his arraylist of received messages
             // And put in the map the sender and his sent message on his arraylist of sent messages
+
+            // Prepare the list of messages of the receiver and the sender
+           // List<Message> receiverMessages = server.getAllUsersMessages().get(publicUserOfTheReceiver(server.getUsersSet(), contactIndex));
+           // List<Message> sentMessages = server.getAllUsersMessages().get(privateUser);
+
+            // Here we have a problem, when i try to get the received messages thats okey cause its a public user, and the map key its a public user
+            // But when I want to update my private list of sentMessages we find that a privateUser and a publicUser are not the same things, so I cannot update
+            // this list.
+            // Maybe we can create a superclass User class that is extended by 2 subclasses which are privateUser and publicUser
+
+            System.out.println("\nThe message has been sent succesfully!\n");
         }
+    }
+
+    private PublicUser publicUserOfTheReceiver(Set<PublicUser> usersSet, int contactIndex) {
+        Iterator iterator = usersSet.iterator();
+        PublicUser userRes = (PublicUser) iterator.next();
+        int index = 0;
+
+        while (index != contactIndex) {
+            userRes = (PublicUser) iterator.next();
+            index++;
+        }
+        return  userRes;
     }
 
     public void read(){
@@ -85,7 +105,8 @@ public class App {
 
     // Auxiliary methods
     public void createUser(){
-        System.out.println("\nType your user name: \n");
+        System.out.println("\nLets create your private user...\n");
+        System.out.println("Type your user name: ");
         Scanner scanner = new Scanner(System.in);
         if (scanner.hasNext()) {
             Random random = new Random();
@@ -95,10 +116,9 @@ public class App {
 
     // Not real methods, just to test the program
     private static void addRandomsUsers() {
-        Map<PublicUser, MessagesPair> auxMap =  server.getAllUsersMessages();
-        auxMap.put(new PublicUser("Lucas", new KeyPair(2,5)), new MessagesPair());
-        auxMap.put(new PublicUser("Danichelo", new KeyPair(1,6)), new MessagesPair());
-        server.setAllUsersMessages(auxMap);
+        server.addUser(new PublicUser("Lucas", new KeyPair(2,5)));
+        server.addUser(new PublicUser("Danichelo", new KeyPair(1,6)));
+        server.addUser(new PublicUser("Tolo", new KeyPair(5,6)));
     }
 
     private String nameOfTheReceiver(Set<PublicUser> publicUsers, int contactIndex) {
